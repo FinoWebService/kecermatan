ebdbe321617ffca5d5e41bc5f28463156f7d065c
FinoWebService <dafinoabimanyups@gmail.com>
2026-01-31 23:28:26 +0700

    Hello

--- firebase.js
+++ firebase.js
@@ -1,58 +1,34 @@
-// ===============================
-// IMPORT FIREBASE SDK
-// ===============================
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
 
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  serverTimestamp
-} from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
-
-
-// ===============================
-// KONFIG FIREBASE
-// ===============================
 const firebaseConfig = {
   apiKey: "AIzaSyBvAuX66peHvmpKtvdh9MIk9Em3WnxDLLU",
   authDomain: "cat-kecermatan-cb9d1.firebaseapp.com",
   projectId: "cat-kecermatan-cb9d1",
   storageBucket: "cat-kecermatan-cb9d1.firebasestorage.app",
   messagingSenderId: "1081198055610",
-  appId: "1:1081198055610:web:d5e9de4f650d86fce57283",
-  measurementId: "G-6F8G5PN134"
+  appId: "1:1081198055610:web:d5e9de4f650d86fce57283"
 };
 
-
-// ===============================
-// INIT FIREBASE
-// ===============================
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-
-// ===============================
-// FUNGSI SIMPAN NILAI
-// ===============================
+// Export function supaya bisa dipakai di file lain
 export async function simpanNilai(nama, nilai){
 
   try {
 
-    await addDoc(collection(db, "hasil_ujian"), {
-
+    await addDoc(collection(db, "hasilTes"), {
       nama: nama,
       nilai: nilai,
-      waktu: serverTimestamp()
-
+      waktu: new Date()
     });
 
-    console.log("Data masuk Firebase!");
+    console.log("BERHASIL SIMPAN!");
 
-  } catch (error){
-
-    console.error("Error simpan:", error);
-
+  } catch (e) {
+    console.error("GAGAL SIMPAN:", e);
   }
 
 }
+simpanNilai("Tes Manual", 88);
